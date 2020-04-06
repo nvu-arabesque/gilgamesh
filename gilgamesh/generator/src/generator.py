34efcb5e143c799e8d32dc60b862781fa94da19a
PC-ubuntu <vuhoang1609@gmail.com>
2020-04-06 15:28:16 +0100

    Restructured working dir for most convenient access

--- gilgamesh/generator/src/generator.py
+++ gilgamesh/generator/src/generator.py
@@ -2,11 +2,7 @@ import os
 import sys
 import numpy as np
 import networkx as nx
-import abstract_graph
-import line_graph
-import complete_graph
+import graphs
 
 types_of_graphs = AbstractGraph.__subclasses__()
-
-
 print(types_of_graphs)
