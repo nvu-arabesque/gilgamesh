266ea0d3e618eb5792062847fb59cceaa63c6e06
affNVu <vtngoc90@gmail.com>
2020-04-13 15:18:22 +0100

    move size to constructor

--- gilgamesh/generator/graphs.py
+++ gilgamesh/generator/graphs.py
@@ -11,7 +11,7 @@ from abc import ABC, abstractmethod
 class AbstractGraph(ABC):
     """ Skeleton for problems """
 
-    def __init__(self):
+    def __init__(self, size):
         self._graph = None
         self._adj_matrix = None
         self._size = None
@@ -20,6 +20,7 @@ class AbstractGraph(ABC):
         self._node_positions = None
         self._edge_list = None
         self._edge_features = None
+        self._label = None
 
     @abstractmethod
     def is_graph_valid(self):
@@ -55,33 +56,66 @@ class AbstractGraph(ABC):
         self._edge_features = [x.split(',')[-1] for x in E]
 
     def get_node_features(self):
-        """ """
-        return self._node_features
+        """
+        
+        Returns
+        ----------
+        tuple: (int, int, list)
+            num_nodes, num_node_features, node_feature    
+        """
+        num_nodes = self._size
+        num_node_features = 0 if self._node_features is None else len(self._node_features[0])
+        node_feature = self._node_features
+        return (num_nodes, num_node_features, node_feature)
 
     def get_edge_list(self):
         """ """
         return self._edge_list
 
     def get_edge_attribute(self):
-        """ """
-        return self._edge_features
+        """
+        Returns
+        ---------
+        tuple: (int, int, list)
+            num_edge, num_edge_features, edge_attr
+        """
+        num_edge = len(self._edge_list)
+        num_edge_features = 0 if self._edge_features is None else len(self._edge_features[0])
+        edge_attr = self._edge_features
+        return (num_edge, num_edge_features, edge_attr)
 
     def get_node_position(self):
-        """ """
-        return self._node_positions
+        """
+        Returns
+        ----------
+        tuple: (int, int, list)
+            num_nodes, num_dim, node_pos
+        """
+        num_nodes = self._size
+        num_dim = 0 if self._node_positions is None else len(self._node_positions[0])
+        node_pos = self._node_positions
+        return (num_nodes, num_dim, node_pos)
+
+    def get_label(self):
+        """
+
+        Returns
+        --------
+        label: list
+        """
+        return self._label
 
 class CompleteGraph(AbstractGraph):
 
-    def __init__(self):
-        AbstractGraph.__init__(self)
+    def __init__(self, size):
+        super().__init__(self)
+        self.populate_graph(size)
 
     def populate_graph(self, size):
         self._size = size
         self._graph = nx.complete_graph(size)
         self._adj_matrix = nx.to_numpy_matrix(self._graph, dtype=np.int64)
         
-        self._node_features = [] * self._size
-
     @staticmethod
     def is_graph_valid(G):
         n = G.number_of_nodes()
@@ -91,8 +125,9 @@ class CompleteGraph(AbstractGraph):
         return True
 
 class LineGraph(AbstractGraph):
-    def __init__(self):
-        AbstractGraph.__init__(self)
+    def __init__(self, size):
+        super().__init__(self)
+        self.populate_graph(size)
 
     def populate_graph(self, size):
         self._size = size
@@ -113,8 +148,9 @@ class LineGraph(AbstractGraph):
 
 
 class CycleGraph(AbstractGraph):
-    def __init__(self):
-        AbstractGraph.__init__(self)
+    def __init__(self, size):
+        super().__init__(self)
+        self.populate_graph(size)
 
     def populate_graph(self, size):
         self._size = size
