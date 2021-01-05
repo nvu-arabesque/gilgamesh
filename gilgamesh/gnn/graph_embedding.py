2f7ff249e8c859cf0e78f62ed29cb8c8d77f11c6
nvu-arabesque <ngoc.vu@arabesque.com>
2021-01-05 13:21:17 +0000

    added reminder

--- gilgamesh/gnn/graph_embedding.py
+++ gilgamesh/gnn/graph_embedding.py
@@ -1,3 +1,17 @@
 """
     Provide some model for graph embedding task.
 """
+
+import gilgamesh
+
+# ======================================================================== 
+#       Embeddings
+# ======================================================================== 
+
+
+TODO: deepwalk, node2vec,line, GraRep
+[Cao et al., 2015], 
+matdix;HOPE [Ou et al., 2016], M-NMF [Wang
+et al., 2017b] 
+deep: SDNE [Wang et al., 2016] and DNGR
+[Cao et al., 2016]
